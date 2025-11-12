c8cb1c80612cb2477ca4572fd050bdf7c0e49c7c
Dimas <dimasherdiansyah002@outlook.com>
2025-11-12 23:00:49 +0700

    Update MainMenu
    Update MainMenu
    Sama Pindahin folder SFX

--- Assets/Scripts/UIScript/MainMenuManager.cs
+++ Assets/Scripts/UIScript/MainMenuManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement; // <-- WAJIB untuk pindah scene
+
 
 
 public class MainMenuManager : MonoBehaviour
@@ -13,12 +15,25 @@ public class MainMenuManager : MonoBehaviour
     public GameObject creditsPanel;
     public GameObject optionsPanel;
     public GameObject dimBackground;
+    public Slider musicSlider;
     // 1. Fungsi untuk Tombol START
     //    Pastikan "NamaSceneGameAnda" sesuai nama file scene game Anda.
     public void StartGame()
     {
         // Ganti "Level_1" dengan nama scene game Anda
         SceneManager.LoadScene("Level_1");
+
+        float initialVolume = 1.0f;
+
+        // 1. Atur UI Slider-nya secara manual ke nilai itu
+        if (musicSlider != null)
+        {
+            musicSlider.value = initialVolume;
+        }
+
+        // 2. Panggil fungsi SetMusicVolume untuk
+        //    mengatur Audio Mixer-nya ke nilai itu juga
+        SetMusicVolume(initialVolume);
     }
     // 3. Fungsi untuk Tombol CREDITS
     public void OpenCredits()
@@ -44,7 +59,25 @@ public class MainMenuManager : MonoBehaviour
         optionsPanel.SetActive(false);
         dimBackground.SetActive(false);
     }
+    public void ToggleFullscreen()
+    {
+        // Baris ini adalah kuncinya.
+        // Screen.fullScreen = !Screen.fullScreen;
+        // Ini membaca status fullscreen saat ini (true/false)
+        // dan mengaturnya ke nilai KEBALIKANNYA.
 
+        // Versi yang lebih mudah dibaca:
+        if (Screen.fullScreen == true)
+        {
+            // Jika sedang fullscreen, buat jadi windowed
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            // Jika sedang windowed, buat jadi fullscreen
+            Screen.fullScreen = true;
+        }
+    }
     // 5. Fungsi untuk SLIDER MUSIK
     // Fungsi ini akan dipanggil SETIAP KALI slider digerakkan
     public void SetMusicVolume(float volume)
@@ -55,7 +88,7 @@ public class MainMenuManager : MonoBehaviour
         // menjadi desibel (dB), cara telinga mendengar.
         mainMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
     }
-    // 2. Fungsi untuk Tombol QUIT
+
     public void QuitGame()
     {
         Debug.Log("QUIT GAME!"); // Pesan ini untuk testing di Editor
